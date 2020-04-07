8e28b849a096764e58ba8845005aeb5c398f1a27
Nicholas Chong <nchong128@gmail.com>
2020-04-07 10:50:29 +1000

    Remove unneeded files. Change to python3

--- export_all_questions.py
+++ export_all_questions.py
@@ -6,7 +6,6 @@ def update_question_links(question_links):
   with open('question_links.txt') as f:
     links =  f.read()
 
-
   links = links.split('\n')
 
   for each in links:
@@ -18,21 +17,21 @@ def get_question(question_link):
   
   resp = requests.get(question_link)
   soup = BeautifulSoup(resp.text, 'html.parser')
-  print '------------------------------'
-  print soup.title.string.split('|')[0]
-  print '------------------------------'
+  print('------------------------------')
+  print(soup.title.string.split('|')[0])
+  print('------------------------------')
 
   return soup.find_all('meta')[1].find_all('meta')[1]['content']
-
 
 
 def main():
   question_links = []
   update_question_links(question_links)
+
   for each_question_link in question_links:
     try:
-      print get_question(each_question_link)
-      print '------------------------------'
+      print(get_question(each_question_link))
+      print('------------------------------')
 
     except:
       continue
